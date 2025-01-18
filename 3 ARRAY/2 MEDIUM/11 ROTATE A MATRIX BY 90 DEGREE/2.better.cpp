d846a4d2efa2dcf25e69c631ecdbe7c341f8858f
Aniketh Singh <aniketh9410280466@gmail.com>
2025-01-18 16:57:37 +0530

    .

---
+++ 3 ARRAY/2 MEDIUM/11 ROTATE A MATRIX BY 90 DEGREE/2.better.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> rotate(vector<vector<int>>mat){
+    int n=mat.size();
+    //see the logic in copy or notes or lec  because it will cover all element
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+        
+    }
+    for(int i=0;i<n;i++){
+        reverse(mat[i].begin(),mat[i].end());
+    }
+    return mat;
+}
+int main(){
+    vector<vector<int>>mat={{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>>ans=rotate(mat);
+    for(auto it:ans){
+        for(auto el:it){
+            cout<<el<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
+//TC is O(n^2+N/2) SC is O(1)
