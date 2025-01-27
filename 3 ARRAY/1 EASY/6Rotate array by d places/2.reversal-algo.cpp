c358d136685a4b32dd7e9c28dc338bc8990dc7a9
Aniketh Singh <aniketh9410280466@gmail.com>
2025-01-27 17:51:08 +0530

    .
    adding...

--- 3 ARRAY/1 EASY/6Rotate array by d places/2.reversal-algo.cpp
+++ 3 ARRAY/1 EASY/6Rotate array by d places/2.reversal-algo.cpp
@@ -8,7 +8,7 @@ void Reverse(int arr[], int start, int end)
     // int temp = arr[start];
     // arr[start] = arr[end];
     // arr[end] = temp;
-    swap(arr[start],arr[end]);
+    swap(arr[start], arr[end]);
     start++;
     end--;
   }
@@ -24,12 +24,13 @@ void Rotatetoleft(int arr[], int n, int k)
   Reverse(arr, 0, n - 1);
 }
 
-int main(){
+int main()
+{
   int n = 7;
   int arr[] = {1, 2, 3, 4, 5, 6, 7};
-  int k ;
-  cout<<"enter the number of rotation"<<endl;
-  cin>>k;
+  int k;
+  cout << "enter the number of rotation" << endl;
+  cin >> k;
   Rotatetoleft(arr, n, k);
   cout << "After Rotating the elements to left " << endl;
   for (int i = 0; i < n; i++)
