b3332350fa6953c43ff3e2f4aaeb5fe70351cfbc
Aniketh Singh <aniketh9410280466@gmail.com>
2025-02-24 10:23:06 +0530

    .

--- 4 BINARY SEARCH/1 BS ON 1D ARRAY/13.FIND PEAK ELEMENT/2.bs.cpp
+++ 4 BINARY SEARCH/1 BS ON 1D ARRAY/13.FIND PEAK ELEMENT/2.bs.cpp
@@ -1,9 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int findPeakElement(vector<int> &arr) {
-    int n=arr.size();
-    if(n==1)return 0;
-    if(arr[0]>arr[1])return 0
+int findPeakElement(vector<int> &arr)
+{
+    int n = arr.size();
+    if (n == 1)
+        return 0;
+        //fo decreasing peak
+    if (arr[0] > arr[1])
+        return 0;
+        //for increasing peak
+    if (arr[n - 1] > arr[n - 2])
+        return n - 1;
+    int low = 1, high = n - 2;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
+        {
+            return mid;
+        }
+        // you are on left side of the peak
+        if (arr[mid] > arr[mid - 1])
+        {
+            low = mid + 1;
+        }
+        //you are on right side
+        //you can combine both the below case in one else case of high=mid-1;
+        else if (arr[mid] > arr[mid + 1])
+        {
+            high = mid - 1;
+        }
+        //for multiple peak go anywhere left or right
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }
 
 int main()
@@ -13,4 +46,4 @@ int main()
     cout << "The peak is at index: " << ans << "\n";
     return 0;
 }
-
+//TC is O(logn)
